a158883fbd86af25589d4408d1988eab458db976
noahanderson3 <noahanderson57@gmail.com>
2022-02-14 23:39:25 -0500

    Deploy

---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const projects = [
+  {
+    title: "Binary Converter",
+    subtitle: "Converts Binary, Hexadecimal, Octal, and Decimal. ",
+    description:
+      "I made this simple number conversion project as a way to better understand how to perform the calculations while learning about it in my CIS 350 course",
+    image: "./binaryGIF.gif",
+    link: "https://github.com/noahanderson3/BinaryConverter.git",
+  },
+  {
+    title: "2-Player Hangman Game",
+    subtitle: "Simple 2-Player or Single-Player Hangman Game",
+    description: "I made this game to practice logic while taking the CIS 199 course. This game allows you have 2 players, one chooses the word and the other guesses, or a single player can choose a catergory and have a randomized word be chosen.",
+    image: "./medium-gray.png",
+    link:"https://github.com/noahanderson3/HangManTwo",
+  },
+  {
+    title:"Tic Toe Toe Game with Computer",
+    subtitle: "Traditional Tic Tac Toe Game with a Computer Opponent",
+    description: "This is the traditional Tic Tac Toe game that allows you to compete against a computer opponent. The logic used for the computer oppenent focuses on defense first and foremost in order to win.",
+    image: "./tictactoe.jpg",
+    link:"https://github.com/noahanderson3/TicTacToeBot",
+  }
+];
+
+export const schoolWorks = [
+
+];
+
+export const skills = [
+  "U.S. Accounting",
+  "VBA for Excel",
+  "Power Platform for Microsoft",
+  "SharePoint",
+  "C#",
+];
